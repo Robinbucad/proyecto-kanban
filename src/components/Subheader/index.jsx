ee656b1588d14cb57a9d3ab3e1478697aa16ee01
Robinbucad <robin.bucad6@gmail.com>
2022-02-24 00:26:40 +0100

    function date

--- src/components/Subheader/index.jsx
+++ src/components/Subheader/index.jsx
@@ -1,12 +1,22 @@
+import { useEffect, useState } from 'react'
 import './style.css'
 
 function SubHeader(){
+/**Esto lo metere en una funcion para que actualize la fecha cuando actualizo una card */
+    let fecha = new Date()
+    let month = fecha.toLocaleString('default', {month:'short'})
+    let day = fecha.getDate()
+
+
+  
+
+ 
 
     return(
         <section className="sub-header">
             <div className='update-info-subheader'>
                 <h3>Version 1.0</h3>
-                <p>Updated on 12 Apr</p>
+                <p>Updated on {day} {month}</p>
             </div>
             <input type='text' className='input-filter' placeholder='Filter cards'></input>
         </section>
