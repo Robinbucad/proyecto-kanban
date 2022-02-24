948b99f3b89f5e7e2d306f4676401445868d99a4
Korobajr <jrcoroba@hotmail.com>
2022-02-24 21:09:14 +0100

    Header and card in  progress

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import './App.css';
 import CreateCard from './components/Card-Done';
 import SubHeader from './components/Subheader';
 import CardToDo from './components/Card-To-Do';
+import Header from './components/Header';
+import CardIndProgress from './components/Card-In-Progress';
 
 function App() {
   return (
     <div className="App">
-      <header>Aqui va el header de Juan</header>
+      <Header></Header>
       <SubHeader></SubHeader>
       <main className='card-container'>
       <CardToDo></CardToDo>
-      <p>Aqui va card progress</p>
+      <CardIndProgress></CardIndProgress>
       <p>Aqui va card Done</p>
       </main>
     </div>
