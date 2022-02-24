763b987daa047b162aafbf98147c177003070ea2
Robinbucad <robin.bucad6@gmail.com>
2022-02-24 22:18:42 +0100

    function time

---
+++ src/components/Card-List-Done/index.jsx
@@ -0,0 +1,17 @@
+
+
+function CardList() {
+
+    
+
+    return (
+    <div className='tarea'>
+        <h4 className='title-task'>Title Tarea</h4>
+        <p>Fecha creacion</p>
+    </div>
+
+    )
+
+}
+
+export default CardList
--- src/components/Card-To-Do/index.jsx
+++ src/components/Card-To-Do/index.jsx
@@ -1,23 +1,75 @@
 import { useState } from 'react'
 import './style.css'
+import CardList from '../Card-List-Done'
 
-function CardToDo(){
+function CardToDo() {
 
-    /**Funcion que enseña la opcion de añadir tareas */
-
+    /**Variable que guarda el Area Text, se usa para mostrar y esconder  */
     const [tarea, updateTarea] = useState([])
-    
-    const handleAdd = () => {
-        updateTarea(true)
-        if(tarea === true){
-            updateTarea(false)
-            console.log('oculta')
-        }else {   
-            console.log('enseña el texto')
-        }     
+
+    /**Esto añade una clase al boton add para darle opacidad si no hay nada escrito */
+    const [opacity, updateOpacity] = useState('opacity')
+
+    /**Varibale que guarda el contenido del texto */
+    const [tareaText, updateTareaText] = useState([])
+    /**Varibale que hace un array de la lista */
+    const [list, updateList] = useState([])
+
+
+    let fecha = new Date()
+    let month = fecha.getMonth()
+    let day = fecha.getDate()
+    let year = fecha.getFullYear()
+    let hour = fecha.getHours()
+    let minutes = fecha.getMinutes()
+    let seconds = fecha.getSeconds()
+
+
+    let secondsSave = localStorage.setItem('seconds', seconds)
+    let minutesSave = localStorage.setItem('minutes', minutes)
+    let hoursSave = localStorage.setItem('hours', hour)
+
+    let takeSeconds = localStorage.getItem('seconds')
+
+
+
+
+    const handleAddTask = e => {
+
+        {
+            tareaText === '' ? console.log('escribe algo') :
+            list.push(tareaText)
+            updateList(list)
+            updateTareaText('')
+        }
+
     }
 
-    return(
+
+    const handleTextTask = e => {
+        updateTareaText(e.target.value)
+        updateOpacity('newOpacity')
+    }
+
+
+
+
+    const handleAdd = () => {
+        updateTarea(true)
+        if (tarea === true) {
+            updateTarea(false)
+            updateTareaText('')
+            console.log('oculta')
+        } else {
+            console.log('enseña el texto')
+        }
+    }
+
+
+    console.log(tareaText)
+
+
+    return (
         <article className='card'>
             <header className='header__card'>
                 <div className='left__header__card'>
@@ -29,19 +81,18 @@ function CardToDo(){
             </header>
             <section className='card-list'>
 
-                {tarea === true ?  <div className='add-tarea'>
-                   <textarea rows='4'className='text-area'></textarea>
+                {tarea === true ? <div className='add-tarea'>
+                    <textarea rows='4' className='text-area' onChange={handleTextTask}></textarea>
                     <div className='btn-divs'>
-                        <button className='btn-tarea-add'>Add</button>
+                        <button className='btn-tarea-add' id={opacity} onClick={handleAddTask}>Add</button>
                         <button className='btn-tarea-cancel'>Cancel</button>
                     </div>
                 </div> : ''}
 
-                <div className='tarea'>
-                    <h4 className='title-task'>Title Tarea</h4>
-                    <p>Fecha creacion</p>
-
-                </div>
+                {list.map((e, i) => <div key={i} className='tarea'>
+                    <h4 className='title-task'>{e}</h4>
+                    <p className='time-creation'>{`created on ${day}/${month}/${year} ${hour}:${minutes}:${takeSeconds}`}</p>
+                </div>)}
 
             </section>
         </article>
@@ -49,3 +100,14 @@ function CardToDo(){
 }
 
 export default CardToDo
+
+/**
+ * {list.map((e, i) => <div key={i} className='tarea'>
+                    <h4 className='title-task'>{e}</h4>
+                    <p>Fecha creacion</p>
+                </div>)}
+ */
+
+/**
+ * updateTareaText(e.target.value)
+ */
